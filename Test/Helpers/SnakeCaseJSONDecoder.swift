d3f14b90c4dc9d8b94daaf6c6841cde003e16b8d
id-kuznetsov <ilua.kuznetsov@gmail.com>
2025-02-24 22:50:22 +0300

    add snakeCase JSONDecoder

---
+++ Test/Helpers/SnakeCaseJSONDecoder.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+final class SnakeCaseJSONDecoder: JSONDecoder, @unchecked Sendable {
+    override init() {
+        super.init()
+        keyDecodingStrategy = .convertFromSnakeCase
+    }
+}
