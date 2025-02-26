91deceac91b15d2223f7915a86b5a6a0e5e04aae
id-kuznetsov <ilua.kuznetsov@gmail.com>
2025-02-26 23:41:25 +0300

    add final class ReviewPhotosCollectionView: UICollectionView { add empty ReviewCollection

---
+++ Test/Screens/Reviews/Cells/ReviewPhotosCollectionView.swift
@@ -0,0 +1,13 @@
+//
+//  ReviewPhotosCollectionView.swift
+//  Test
+//
+//  Created by Ilya Kuznetsov on 26.02.2025.
+//
+
+import UIKit
+
+final class ReviewPhotosCollectionView: UICollectionView {
+
+
+}
